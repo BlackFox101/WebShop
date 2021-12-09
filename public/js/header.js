fa9c837700573f2593d20f87c4d17c81ed10a109
Insaf4ik <insaf.halirahmanoff@yandex.ru>
2021-12-09 22:59:46 +0300

    Стилизовал header

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,8 @@
-const findBtn = document.querySelector('.header__search-find-btn')
+const findBtn = document.querySelector('.find-button')
 const select = document.querySelector('.header__search-select')
 const logoutBtn = document.querySelector('.header__container-logout-btn')
+const authMenuBtn = document.querySelector('.auth-menu')
+const authMenuList = authMenuBtn.querySelector('ul')
 
 let selectedCategory = select.value
 
@@ -15,4 +17,23 @@ logoutBtn.addEventListener('click', async () => {
 findBtn.addEventListener('click', () => {
     // TODO
     console.log('clicked', selectedCategory)
+})
+
+authMenuBtn.addEventListener('click', (e) => {
+    if (authMenuList.style.display === 'none') {
+        authMenuBtn.style.right = '-132px'
+        authMenuList.style.display = 'block'
+    }
+    else if (e.target.parentNode.className !== 'auth-menu__list') {
+        authMenuBtn.style.right = '-75px'
+        authMenuList.style.display = 'none'
+    }
+})
+
+window.addEventListener('click', (e) => {
+    console.log(e.target.parentNode)
+    if (!['auth-menu__list', 'user-name', 'auth-menu'].includes(e.target.parentNode.className)) {
+        authMenuBtn.style.right = '-75px'
+        authMenuList.style.display = 'none'
+    }
 })
